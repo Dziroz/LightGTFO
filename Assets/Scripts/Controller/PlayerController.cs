8bc0bb08e0c8378a8b1ffe33b203b457dc98e573
Dziroz <abcdanil@bk.ru>
2024-07-10 17:44:25 +0500

    Movement, Rotation, ProBuilder, ProGrids

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float playerSpeed = 5f;
+    [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private float controllerDeadZone = 0.1f;
+    [SerializeField] private float gamepadRotateSmoothing = 1000f;
+
+    private CharacterController controller;
+
+    private Vector2 movement;
+    private Vector2 aim;
+
+    private Vector3 playerVelocity;
+
+    private PlayerControls playerControls;
+    private PlayerInput playerInput;
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        playerControls = new PlayerControls();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+    void Update()
+    {
+        HandleInput();
+        HandleMovement();
+        HandleRotation();
+    }
+    void HandleInput()
+    {
+        movement = playerControls.Player.Move.ReadValue<Vector2>();
+        aim = playerControls.Player.Aim.ReadValue<Vector2>();
+    }
+    void HandleMovement()
+    {
+        Vector3 move = new Vector3(movement.x, 0, movement.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+    }
+    void HandleRotation()
+    {
+        if(Mathf.Abs(aim.x) > controllerDeadZone || Mathf.Abs(aim.y) > controllerDeadZone)
+        {
+            Vector3 playerDirection = Vector3.right * -aim.y + Vector3.forward * aim.x;
+            Debug.Log(playerDirection);
+            if(playerDirection.sqrMagnitude > 0.0f)
+            {
+                Quaternion newrotation = Quaternion.LookRotation(playerDirection, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, newrotation, gamepadRotateSmoothing * Time.deltaTime);
+            }
+        }
+    }
+}
