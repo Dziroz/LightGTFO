34d8a02a03fa1c324e5587d463f4a9b2ed43e12f
Dziroz <abcdanil@bk.ru>
2024-08-19 23:28:26 +0500

    Spawner

--- Assets/Scripts/Enemy/EnemySpawn.cs
+++ Assets/Scripts/Enemy/EnemySpawn.cs
@@ -4,23 +4,78 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
+
     [SerializeField] private int maxEnemyOnScene;
-    [SerializeField] private int EnemyOnScene;
+    private int EnemyOnScene;
     [SerializeField] GameObject[] enemyPrefab;
-    //[SerializeField] private int 
+    [SerializeField]  int EnemyCount;
+
+    [SerializeField] float SpawnColdown;
+    [SerializeField] float spawningDistance;
+
+    [SerializeField] int Snake;
+    [SerializeField] int Tank;
+    
+    float timer;
     void Start()
     {       
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        timer += Time.deltaTime;
+        if(timer >= SpawnColdown)
+        {
+            if(GameObject.FindGameObjectsWithTag("Enemy").Length < maxEnemyOnScene)
+            {
+                Spawn();
+                timer = 0;
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
     }
     void Spawn()
     {
-        Instantiate(enemyPrefab[0]);
-        EnemyOnScene++;
+        if (EnemyCount == Snake)
+        {
+            Instantiate(enemyPrefab[1], (Position()), Quaternion.identity);
+            EnemyCount++;
+            EnemyOnScene++;              
+        }
+        else
+        {
+            Instantiate(enemyPrefab[0], (Position()), Quaternion.identity);
+            EnemyCount++;
+            EnemyOnScene++;
+        }
+        if (EnemyCount == Tank)
+        {
+            Instantiate(enemyPrefab[2], (Position()), Quaternion.identity);
+            EnemyCount++;
+            EnemyOnScene++;
+        }
+        else
+        {
+            Instantiate(enemyPrefab[0], (Position()), Quaternion.identity);
+            EnemyCount++;
+            EnemyOnScene++;
+        }
+        if(EnemyCount >= 31)
+        {
+            EnemyCount = 0;
+        }
     }
-    
+    Vector3 Position()
+    {
+        Vector3 startPosition = new Vector3(Camera.main.transform.position.x, 0, Camera.main.transform.position.z);
+        float x = Random.Range(-1.0f, 1.0f);
+        float z = Random.Range(-1.0f, 1.0f);
+        Vector3 v = new Vector3(x, 0.0f, z).normalized;
+        Vector3 pos = startPosition + v * spawningDistance;
+        return pos;
+
+    }
+
 }
