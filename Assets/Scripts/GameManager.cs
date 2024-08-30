a211a24f2175a9898d1a5f4616507a73efdff530
Dziroz <abcdanil@bk.ru>
2024-08-30 20:01:59 +0500

    131

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool game;
+    public GameObject[] players;
+    public bool[] PlayerStatus;
+    public int liveCounter;
+    public FireManager fireManagerScript;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        FindPlayer();
+        FindPlayerLiveStatus();
+    }
+    void FindPlayerLiveStatus()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            PlayerStatus[i] = players[i].GetComponent<PlayerController>().alive;
+            if(PlayerStatus[i] == false)
+            {
+                liveCounter++;
+            }
+            if(liveCounter == players.Length)
+            {
+                End();
+            }
+
+
+        }
+        liveCounter = 0;
+    }
+    void FindPlayer()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        PlayerStatus = new bool[players.Length];
+    }
+    void End()
+    {
+        if(fireManagerScript.firePower <= 0)
+        {
+            Debug.Log("End");
+        }
+        Debug.Log("End");
+    }
+}
